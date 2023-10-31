77bd6d6a7202317d47d435a5286c87c441332c45
EMILY <eguaycha2431@utm.edu.ec>
2023-10-30 21:43:34 -0500

    Commit 2: Método para configuración inicial

--- src/main/java/com/mycompany/evelinpoo1/DeberCAI.java
+++ src/main/java/com/mycompany/evelinpoo1/DeberCAI.java
@@ -12,6 +12,17 @@ public class DeberCAI {
         random = new Random();
         aciertos = 0;
         intentos = 0;
+        
+        
     }
+    
+        public void configuracionInicial() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Elige el nivel de dificultad (1, 2, ...): ");
+        nivelDificultad = scanner.nextInt();
+        System.out.print("Elige el tipo de problema aritmético (1: Suma, 2: Resta, 3: Multiplicación, 4: División, 5: Aleatorio): ");
+        tipoProblema = scanner.nextInt();
+    }
+
 }
 
