bef8c2c0c51986b4a166e1d7ae07da2fcc98fc0b
EMILY <eguaycha2431@utm.edu.ec>
2023-10-30 21:53:38 -0500

    Commit 4: Métodos para generar mensajes positivos y negativos

--- src/main/java/com/mycompany/evelinpoo1/DeberCAI.java
+++ src/main/java/com/mycompany/evelinpoo1/DeberCAI.java
@@ -30,6 +30,15 @@ public class DeberCAI {
         int num2 = random.nextInt((max - min) + 1) + min;
         return new int[]{num1, num2};
     }
+    public void generarMensajePositivo() {
+        String[] mensajes = {"¡Muy bien!", "¡Excelente!", "¡Buen trabajo!", "¡Sigue así!"};
+        System.out.println(mensajes[random.nextInt(mensajes.length)]);
+    }
+
+    public void generarMensajeNegativo() {
+        String[] mensajes = {"No. Por favor intenta de nuevo.", "Incorrecto. Intenta una vez más.", "¡No te rindas!", "No. Sigue intentando."};
+        System.out.println(mensajes[random.nextInt(mensajes.length)]);
+    }
 
 
 }
